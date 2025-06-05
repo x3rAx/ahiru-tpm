86107bfc38ca693f15f8662958251e9919628c38
^x3ro <git@x3ro.dev>
2025-06-05 23:47:48 +0200

    📦 NEW: Get action from cli args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use tpm_rs::plugins::install_plugins;
 
 fn main() -> Result<()> {
-    install_plugins()?;
-    Ok(())
+    // Get first argument
+    let action = std::env::args().nth(1).unwrap_or("install".into());
+
+    match action.as_ref() {
+        "install" => install_plugins(),
+        _ => Err(anyhow!("Unknown action: {action}")),
+    }
 }
