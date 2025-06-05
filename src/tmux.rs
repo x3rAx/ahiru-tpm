ef8f5df1fe59898e9f319ec93ab842f20cc1b34b
^x3ro <git@x3ro.dev>
2025-06-05 18:05:22 +0200

    📦 NEW: Add function to get config dir

--- src/tmux.rs
+++ src/tmux.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use cmd_lib::run_fun;
 
 pub fn get_option(name: &str) -> Option<String> {
@@ -7,20 +9,29 @@ pub fn get_option(name: &str) -> Option<String> {
 // TODO:
 //   - Also load from `/etc/tmux.conf`
 //   - Get sourced files from tmux and also load from them
-pub fn get_config_path() -> Option<std::path::PathBuf> {
+pub fn get_config_path() -> Option<PathBuf> {
+    get_config_dir().map(|p| p.join("tmux.conf"))
+}
+
+pub fn get_config_dir() -> Option<PathBuf> {
     // Try `$XDG_CONFIG_HOME` (with fallback to `$HOME/.config/`)
-    if let Some(config_path) = xdir::config().map(|path| path.join("tmux/tmux.conf")) {
-        if config_path.exists() {
-            return Some(config_path);
-        }
+    let config_dir = xdir::config().map(|path| path.join("tmux"));
+    if has_tmux_config(&config_dir) {
+        return config_dir;
     }
 
     // Try `$HOME/.tmux`
-    if let Some(config_path) = xdir::home().map(|path| path.join("tmux/tmux.conf")) {
-        if config_path.exists() {
-            return Some(config_path);
-        }
+    let config_dir = xdir::home().map(|path| path.join(".tmux"));
+    if has_tmux_config(&config_dir) {
+        return config_dir;
     }
 
     None
+}
+
+fn has_tmux_config(config_dir: &Option<PathBuf>) -> bool {
+    if let Some(config_path) = config_dir.as_ref().map(|p| p.join("tmux.conf")) {
+        return config_path.exists();
+    }
+    false
 }
