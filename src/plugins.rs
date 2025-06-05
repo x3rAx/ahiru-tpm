c60c560560b3edbbd7f33349a9ece42b68884ca5
^x3ro <git@x3ro.dev>
2025-06-05 10:39:48 +0200

    👌 IMPROVE: Refactor `load_specs_from_config`

--- src/plugins.rs
+++ src/plugins.rs
@@ -32,27 +32,39 @@ fn load_specs_from_config(config_path: &Path) -> Result<Vec<String>> {
 
     reader
         .lines()
-        .filter_map(|line| match line {
-            Ok(l) => re
-                .captures(&l)
-                .and_then(|caps| caps.name("spec").map(|m| Ok(m.as_str().to_string()))),
-            Err(e) => Some(Err(e)),
-        })
+        .filter_map(filter_and_get_raw_plugin_spec(re))
         .collect::<Result<Vec<_>, _>>()?
         .into_iter()
-        .map(|s| match QuotedParser::parse(Rule::quoted_string, &s) {
-            Ok(mut pairs) => Ok(pairs
-                .next()
-                .expect("The first pair should always exist")
-                .into_inner()
-                .next()
-                .expect("The single or double quoted string should always exist")
-                .into_inner()
-                .next()
-                .expect("The single or double inner should always exist")
-                .as_str()
-                .to_string()),
-            Err(e) => Err(e).context("Failed to parse plugins from tmux config"),
-        })
+        .map(parse_quoted_string)
         .collect::<Result<_, _>>()
+}
+
+fn filter_and_get_raw_plugin_spec(
+    re: Regex,
+) -> impl FnMut(
+    std::result::Result<String, std::io::Error>,
+) -> Option<std::result::Result<String, std::io::Error>> {
+    move |line| match line {
+        Ok(l) => re
+            .captures(&l)
+            .and_then(|caps| caps.name("spec").map(|m| Ok(m.as_str().to_string()))),
+        Err(e) => Some(Err(e)),
+    }
+}
+
+fn parse_quoted_string(s: String) -> std::result::Result<String, anyhow::Error> {
+    match QuotedParser::parse(Rule::quoted_string, &s) {
+        Ok(mut pairs) => Ok(pairs
+            .next()
+            .expect("The first pair should always exist")
+            .into_inner()
+            .next()
+            .expect("The single or double quoted string should always exist")
+            .into_inner()
+            .next()
+            .expect("The single or double inner should always exist")
+            .as_str()
+            .to_string()),
+        Err(e) => Err(e).context("Failed to parse plugins from tmux config"),
+    }
 }
