1e731f8a43e30a835097d37d8dd86d9db60bc923
^x3ro <git@x3ro.dev>
2025-06-05 23:39:15 +0200

    👌 IMPROVE: Remove dummy code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 use anyhow::Result;
-use tpm_rs::{
-    plugins::{self, install_plugins},
-    tmux::{self, get_option},
-};
+use tpm_rs::plugins::install_plugins;
 
 fn main() -> Result<()> {
-    let tpm_async = get_option("@tpm_async").unwrap_or("".into()) == "true";
-
-    println!("@tpm_async: {}", tpm_async);
-
-    let specs = plugins::load_specs()?;
-
-    println!("\nPlugins:");
-    for spec in &specs {
-        println!("  {}", spec.url());
-    }
-
-    println!("\nPlugins dir: {:?}", tmux::get_plugins_dir());
-
     install_plugins()?;
-
-    println!("\nDone\n");
-
     Ok(())
 }
