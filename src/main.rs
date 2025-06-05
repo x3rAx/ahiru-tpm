4e7c150eb7cc1d544eb16f3d59ce8215c9c8f026
^x3ro <git@x3ro.dev>
2025-06-05 20:33:08 +0200

    📦 NEW: Install plugins into temp dir as a PoC

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use anyhow::Result;
-use tpm_rs::{plugins, tmux::get_option};
+use anyhow::{Context, Result};
+use cmd_lib::run_cmd;
+use tpm_rs::{
+    plugins,
+    tmux::{self, get_option},
+};
 
 fn main() -> Result<()> {
     let tpm_async = get_option("@tpm_async").unwrap_or("".into()) == "true";
@@ -9,9 +13,28 @@ fn main() -> Result<()> {
     let specs = plugins::load_specs()?;
 
     println!("\nPlugins:");
-    for spec in specs {
+    for spec in &specs {
         println!("  {}", spec.url());
     }
+
+    println!("\nPlugins dir: {:?}", tmux::get_plugins_dir());
+
+    let _plugins_dir = tmux::get_plugins_dir().context("Failed to get tmux plugins dir")?;
+
+    for spec in specs {
+        let url = spec.url();
+        let name = spec.name();
+        println!("\nInstalling {}", spec.name());
+
+        run_cmd!(
+            cd /tmp;
+            rm -rf tmux-plugins;
+            mkdir -p tmux-plugins;
+            git clone $url tmux-plugins/$name;
+        )?;
+    }
+
+    println!("\nDone\n");
 
     Ok(())
 }
