29d58f28b28e97c03b54cda96ea63072dd4fb545
^x3ro <git@x3ro.dev>
2025-06-08 23:22:09 +0200

    🐛 FIX: Load home config before xdg config

--- src/tmux.rs
+++ src/tmux.rs
@@ -15,16 +15,18 @@ pub fn get_existing_config_paths() -> Vec<PathBuf> {
         configs.push(system_config);
     }
 
-    let user_config = get_user_config_path();
-    if user_config.exists() {
-        configs.push(user_config);
-    }
-
     let home_config = xdir::home()
         .expect("$HOME should be set")
         .join(".tmux.conf");
     if home_config.exists() {
         configs.push(home_config);
+    }
+
+    let user_config = xdir::config()
+        .expect("XDG paths should be possible to build")
+        .join("tmux/tmux.conf");
+    if user_config.exists() {
+        configs.push(user_config);
     }
 
     configs
@@ -48,30 +50,4 @@ pub fn ensure_plugins_dir_exists() -> Result<PathBuf> {
     }
 
     Ok(path)
-}
-
-pub fn get_config_dir() -> PathBuf {
-    get_user_config_path()
-        .parent()
-        .expect("User config is a file and should therefore have a parent")
-        .to_owned()
-}
-
-fn get_user_config_path() -> PathBuf {
-    // Try `$XDG_CONFIG_HOME` (with fallback to `$HOME/.config/`)
-    let config_path_xdg = xdir::config()
-        .expect("XDG paths should be possible to build")
-        .join("tmux/tmux.conf");
-    if config_path_xdg.exists() {
-        return config_path_xdg;
-    }
-
-    // Try `$HOME/.tmux`
-    let config_path = xdir::home().expect("XDG paths should be possible to build");
-    if config_path.exists() {
-        return config_path;
-    }
-
-    // Use `$XDG_CONFIG_HOME` as fallback
-    config_path_xdg
 }
