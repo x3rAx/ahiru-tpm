38ce4c7decdf7ad0371d4ab609bca66052fd3f3a
^x3ro <git@x3ro.dev>
2025-06-12 23:30:22 +0200

    🐛 FIX: Display url from spec, not full url

--- src/plugin.rs
+++ src/plugin.rs
@@ -3,7 +3,7 @@ use std::{fmt::Display, path::PathBuf};
 use once_cell::sync::OnceCell;
 use url::Url;
 
-use crate::{attribute::Attribute, spec::Spec, tmux};
+use crate::{attribute::Attribute, repo_url::RepoUrl, spec::Spec, tmux};
 
 pub struct Plugin {
     spec: Spec,
@@ -15,6 +15,10 @@ pub struct Plugin {
 impl Plugin {
     pub fn url(&self) -> Url {
         self.spec.url().into()
+    }
+
+    pub fn repo_url(&self) -> &RepoUrl {
+        self.spec.url()
     }
 
     pub fn name(&self) -> &str {
@@ -49,8 +53,8 @@ impl From<Spec> for Plugin {
 
 impl Display for Plugin {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} ({}", self.spec.name(), self.spec.url())?;
-        if let Some(branch) = self.spec.branch() {
+        write!(f, "{} ({}", self.name(), self.repo_url())?;
+        if let Some(branch) = self.branch() {
             write!(f, "#{branch}")?;
         };
         write!(f, ")")
