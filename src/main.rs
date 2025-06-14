125388d5f531a8df5d0a042df3b1ded1a78503da
^x3ro <git@x3ro.dev>
2025-06-14 13:26:34 +0200

    👌 IMPROVE: Move `load` logic into separate mod

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<()> {
     match args.action {
         Action::Install(install_arga) => plugins::install::install_cmd(install_arga),
         Action::Update(update_args) => plugins::update_cmd(update_args),
-        Action::Load => plugins::load_cmd(),
+        Action::Load => plugins::load::load_cmd(),
         Action::Clean => plugins::clean_cmd(),
         Action::Sync => plugins::sync_cmd(),
     }
--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,4 +1,5 @@
 pub mod install;
+pub mod load;
 
 use std::{
     collections::{HashMap, HashSet},
@@ -9,9 +10,8 @@ use anyhow::{Context, Result, anyhow};
 use cached::proc_macro::cached;
 use cmd_lib::run_cmd;
 use glob::glob;
-use is_executable::IsExecutable;
 use itertools::Itertools;
-use log::{info, warn};
+use log::warn;
 use rayon::prelude::*;
 
 use crate::{
@@ -84,45 +84,6 @@ pub fn get_plugins() -> Result<Vec<Plugin>> {
     Ok(plugins)
 }
 
-pub fn load_cmd() -> Result<()> {
-    load()?;
-
-    println!();
-    println!("==> Plugins have been reloaded.");
-    Ok(())
-}
-
-fn load() -> Result<()> {
-    tmux::setup_keymaps()?;
-    let plugins = get_plugins()?;
-    let (parallel, non_parallel) = plugins.into_iter().partition::<Vec<_>, _>(|p| p.parallel());
-
-    non_parallel.iter().try_for_each(load_plugin)?;
-    parallel.par_iter().try_for_each(load_plugin)
-}
-
-fn load_plugin(plugin: &Plugin) -> Result<()> {
-    info!("Loading plugin {}", plugin.name());
-
-    let path_str = plugin.path().to_str().context("Path is not valid UTF-8")?;
-
-    // Find all plugin init files (executable files ending in `.tmux`)
-    for entry in glob(&format!("{path_str}/*.tmux"))? {
-        let entry = entry?;
-        if !entry.is_file() || !entry.is_executable() {
-            continue;
-        }
-
-        let init_file = entry.to_str().context("Path is not valid UTF-8")?;
-
-        // Run the init plugin file
-        run_cmd!($init_file)
-            .or_else(|_| run_cmd!(/bin/sh $init_file))
-            .context(format!(r#"Failed to load plugin "{}""#, plugin.name()))?;
-    }
-    Ok(())
-}
-
 pub fn update_cmd(args: UpdateArgs) -> Result<()> {
     if args.all {
         update_all()?;
@@ -131,7 +92,7 @@ pub fn update_cmd(args: UpdateArgs) -> Result<()> {
     }
 
     if args.load {
-        load_cmd()?;
+        load::load_cmd()?;
     }
 
     println!("==> Done updating plugins.");
@@ -233,7 +194,7 @@ pub fn clean() -> Result<()> {
 
 pub fn sync_cmd() -> Result<()> {
     sync()?;
-    load()?;
+    load::load()?;
 
     println!();
     println!("==> Done syncing plugins.");
