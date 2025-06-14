ef70eded5ad5d976f83eb6017c3f3a8fb0bdb99a
^x3ro <git@x3ro.dev>
2025-06-15 01:39:16 +0200

    👌 IMPROVE: Return InstallState to indicate wheter a plugin was skipped or installed

--- src/plugins/install.rs
+++ src/plugins/install.rs
@@ -1,24 +1,39 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Error, Result};
 use cmd_lib::run_cmd;
 use futures::{StreamExt, stream::FuturesUnordered};
 use tokio::task;
 
 use crate::{plugin::Plugin, tmux::ensure_plugins_dir_exists};
 
+enum InstallResult {
+    Installed,
+    Skipped,
+}
+
 pub async fn install() -> Result<()> {
     if super::do_parallel() {
         install_parallel().await
     } else {
-        super::get_plugins()?.iter().try_for_each(install_plugin)
+        super::get_plugins()?.iter().try_for_each(|p| {
+            install_plugin(p).context("")?;
+            Ok(())
+        })
     }
 }
 
 async fn install_parallel() -> Result<()> {
-    let mut tasks = FuturesUnordered::new();
+    let mut tasks: FuturesUnordered<task::JoinHandle<std::result::Result<(), Error>>> =
+        FuturesUnordered::new();
     let plugins = super::get_plugins()?;
 
     for plugin in plugins {
-        tasks.push(task::spawn(async move { install_plugin(&plugin) }));
+        tasks.push(task::spawn(async move {
+            match install_plugin(&plugin)? {
+                InstallResult::Installed => println!("Installed: {plugin}"),
+                InstallResult::Skipped => (),
+            };
+            Ok(())
+        }));
     }
 
     while let Some(result) = tasks.next().await {
@@ -28,9 +43,9 @@ async fn install_parallel() -> Result<()> {
     Ok(())
 }
 
-fn install_plugin(plugin: &Plugin) -> Result<()> {
+fn install_plugin(plugin: &Plugin) -> Result<InstallResult> {
     if plugin.is_installed() {
-        return Ok(());
+        return Ok(InstallResult::Skipped);
     }
 
     ensure_plugins_dir_exists()?;
@@ -47,5 +62,7 @@ fn install_plugin(plugin: &Plugin) -> Result<()> {
     run_cmd!(
         GIT_TERMINAL_PROMPT=0 git clone --single-branch --recursive $[branch_args] $url $path;
     )
-    .context(format!(r#"Failed to install plugin "{}""#, plugin.name()))
+    .context(format!(r#"Failed to install plugin "{}""#, plugin.name()))?;
+
+    Ok(InstallResult::Installed)
 }
