5a7be5e4d54dc1bdd1237a14751d53aa60f4deb3
^x3ro <git@x3ro.dev>
2025-06-18 23:08:01 +0200

    👌 IMPROVE: Use inputs from flake when using non-flake `nix-shell`

--- shell.nix
+++ shell.nix
@@ -1,25 +1,39 @@
-{
-  pkgs ? import <nixpkgs> {},
-  fenix ? import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {},
-  fenix-shell-profile ? fenix.stable,
-}:
-pkgs.mkShell {
-  name = "nix-shell";
+let
+  inherit (builtins) currentSystem fromJSON readFile;
 
-  packages = with pkgs; [
-    bashInteractive
+  getFlake = name: let
+    flake = (fromJSON (readFile ./flake.lock)).nodes.${name}.locked;
+  in {
+    inherit (flake) rev;
+    outPath = fetchTarball {
+      url = "https://github.com/${flake.owner}/${flake.repo}/archive/${flake.rev}.tar.gz";
+      sha256 = flake.narHash;
+    };
+  };
+in
+  {
+    system ? currentSystem,
+    pkgs ? import (getFlake "nixpkgs") {localSystem = {inherit system;};},
+    fenix ? import (getFlake "fenix") {},
+    fenix-shell-profile ? fenix.stable,
+  }:
+    pkgs.mkShell {
+      name = "nix-shell";
 
-    (fenix-shell-profile.withComponents [
-        "cargo"
-        "rust-src"
-        "rust-analyzer"
-        "clippy"
-    ])
-    bacon # CLI test runner
-    cargo-watch
+      packages = with pkgs; [
+        bashInteractive
 
-    #openssl.dev
-    #pkgconfig # Required to find openssl
-    #lldb # Install lldb with `lldb-dap` (aka `lldb-vscode`)
-  ];
-}
+        (fenix-shell-profile.withComponents [
+          "cargo"
+          "rust-src"
+          "rust-analyzer"
+          "clippy"
+        ])
+        bacon # CLI test runner
+        cargo-watch
+
+        #openssl.dev
+        #pkgconfig # Required to find openssl
+        #lldb # Install lldb with `lldb-dap` (aka `lldb-vscode`)
+      ];
+    }
