3c44cac9ae66c9e130772dea5d03009467c60121
^x3ro <git@x3ro.dev>
2025-06-18 22:09:16 +0200

    👌 IMPROVE: Switch from `rust-overlay` to `fenix`

--- flake.nix
+++ flake.nix
@@ -5,32 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    # Rust toolchains
+    fenix = {
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Library for building Cargo projects
     crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    rust-overlay,
     crane,
     ...
-  }:
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        overlays = [
-          (import rust-overlay)
-        ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        fenix = inputs.fenix.packages.${system};
 
-        pkgs = import nixpkgs {inherit system overlays;};
+        fenix-shell-profile = fenix.stable;
+        fenix-build-toolchain = fenix.stable.minimalToolchain;
 
-        getRustToolchain = pkgs: pkgs.rust-bin.stable.latest.default;
-
-        craneLib = (crane.mkLib pkgs).overrideToolchain getRustToolchain;
+        craneLib = (crane.mkLib pkgs).overrideToolchain fenix-build-toolchain;
 
         cleanCargoSource = src:
           pkgs.lib.cleanSourceWith {
@@ -42,9 +41,12 @@
               isCargoSource || isPestFile;
           };
       in rec {
-        devShells.default = import ./shell.nix {
-          inherit pkgs;
-          rust-toolchain = getRustToolchain pkgs;
+        devShells = {
+          default = import ./shell.nix {
+            inherit pkgs;
+            inherit fenix;
+            inherit fenix-shell-profile;
+          };
         };
 
         packages = {
@@ -54,7 +56,7 @@
 
         overlays = {
           default = overlays.ahiru-tpm;
-          ahiru-tpm = (_final: _prev: {ahiru-tpm = packages.ahiru-tpm;});
+          ahiru-tpm = _final: _prev: {ahiru-tpm = packages.ahiru-tpm;};
         };
       }
     );
