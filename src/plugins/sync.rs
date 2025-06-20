19db90a034a683930fd844022ca4f4e0680c39d0
^x3ro <git@x3ro.dev>
2025-06-20 12:42:21 +0200

    📦 NEW: Show progress while updating plugins

--- src/plugins/sync.rs
+++ src/plugins/sync.rs
@@ -3,6 +3,6 @@ use anyhow::Result;
 pub async fn sync() -> Result<()> {
     super::install::install().await?;
     super::clean::clean()?;
-    super::update::update_all()?;
+    super::update::update_all().await?;
     Ok(())
 }
